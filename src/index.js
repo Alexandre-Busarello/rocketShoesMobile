bda3beb73c90e525169c2694800ba479f23643ae
Alexandre Busarello <busamar@LAPTOP-NKAJQ9SE.localdomain>
2019-07-17 14:40:52 +0000

    Initialize project rocketshoes mobile

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome to React Native!</Text>
+    </View>
+  );
+}
